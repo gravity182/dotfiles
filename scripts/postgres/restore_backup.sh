f4c90a0e5ba2dd9fa052928d7648a4dc0f0a34bc
blinky-z <dm.blink19@gmail.com>
2024-11-28 18:00:15 +0300

    Update the PostgreSQL backup restore script

--- scripts/postgres/restore_backup.sh
+++ scripts/postgres/restore_backup.sh
@@ -5,9 +5,9 @@ IFS=$'\n\t'
 echo -n "Input database name: "
 read -r database
 
-backup=$(fzf --query=".sql$" --header="Select backup")
+backup=$(fzf --query=".sql$" --header="Select the backup file")
 
-echo "'$database' - '$backup'"
+echo "Restoring backup '$backup' to database '$database'"
 
 PGOPTIONS='--client-min-messages=warning' psql -X -q -1 -v ON_ERROR_STOP=1 --pset pager=off -d "$database" -f "$backup" -L restore_$(date +%Y%m%d_%H%M%S).log
 

8d9d7389e65ad9c11f2a4c310933c8f494dc0a5e
blinky-z <dm.blink19@gmail.com>
2024-11-28 04:54:48 +0300

    Add a Postgres backup restoration script

---
+++ scripts/postgres/restore_backup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+echo -n "Input database name: "
+read -r database
+
+backup=$(fzf --query=".sql$" --header="Select backup")
+
+echo "'$database' - '$backup'"
+
+PGOPTIONS='--client-min-messages=warning' psql -X -q -1 -v ON_ERROR_STOP=1 --pset pager=off -d "$database" -f "$backup" -L restore_$(date +%Y%m%d_%H%M%S).log
+

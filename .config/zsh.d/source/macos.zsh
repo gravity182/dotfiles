3437ff0b3a8cf008da950ab6a8cea97d7a6c35cf
gravity182 <dm.blink19@gmail.com>
2026-01-04 23:32:19 +0300

    zsh(macos): homebrew paths and iTerm integration

--- .config/zsh.d/source/macos.zsh
+++ .config/zsh.d/source/macos.zsh
@@ -1,9 +1,9 @@
-#                        ___  ____  
-#  _ __ ___   __ _  ___ / _ \/ ___| 
-# | '_ ` _ \ / _` |/ __| | | \___ \ 
+#                        ___  ____
+#  _ __ ___   __ _  ___ / _ \/ ___|
+# | '_ ` _ \ / _` |/ __| | | \___ \
 # | | | | | | (_| | (__| |_| |___) |
-# |_| |_| |_|\__,_|\___|\___/|____/ 
-#                                   
+# |_| |_| |_|\__,_|\___|\___/|____/
+#
 
 if ! _is_osx; then
     return 1
@@ -12,20 +12,46 @@ fi
 # GNU Core Utils
 # --------------
 
-for gbin in /opt/homebrew/opt/*/libexec/gnubin; do
-  export PATH="$gbin:$PATH"
-done
-for gman in /opt/homebrew/opt/*/libexec/gnuman; do
-  export MANPATH="$gman:$MANPATH"
-done
+export PATH="${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin:$PATH"
+export PATH="${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnubin:$PATH"
+export PATH="${HOMEBREW_PREFIX}/opt/gawk/libexec/gnubin:$PATH"
+export MANPATH="${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnuman:$MANPATH"
+export MANPATH="${HOMEBREW_PREFIX}/opt/findutils/libexec/gnuman:$MANPATH"
+export MANPATH="${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnuman:$MANPATH"
+export MANPATH="${HOMEBREW_PREFIX}/opt/gawk/libexec/gnuman:$MANPATH"
 
 # ===============
-# MISCELLANEOUS
+# Aliases
 # ===============
 
-if [[ $TERM_PROGRAM =~ "iTerm" ]]; then
-    # integration between zsh and iterm2
-    [[ ! -e "${HOME}/.iterm2_shell_integration.zsh" ]] && curl -L https://iterm2.com/shell_integration/zsh -o "${HOME}/.iterm2_shell_integration.zsh"
-    source "${HOME}/.iterm2_shell_integration.zsh"
-fi
+# put system to sleep
+alias afk='pmset sleepnow'
 
+# caffeinate - keep the mac awake
+alias coffee='caffeinate -i -m -t'
+
+# ===============
+# Homebrew
+# ===============
+
+# print install time
+export HOMEBREW_DISPLAY_INSTALL_TIMES=1
+# disable analytics
+export HOMEBREW_NO_ANALYTICS=1
+# auto-update Homebrew every month on install/upgrade/reinstall
+export HOMEBREW_AUTO_UPDATE_SECS=2592000
+# do not cleanup automatically on install/upgrade/reinstall
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+# disable env hints
+export HOMEBREW_NO_ENV_HINTS=1
+
+# ===============
+# iTerm2
+# ===============
+
+# shell integration for iTerm2
+# https://iterm2.com/documentation-shell-integration.html
+[[ ! -e "${HOME}/.iterm2_shell_integration.zsh" ]] && curl -sSfL https://iterm2.com/shell_integration/zsh -o "${HOME}/.iterm2_shell_integration.zsh"
+export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
+source "${HOME}/.iterm2_shell_integration.zsh"
